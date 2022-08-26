b783f1e05197e9e0a5387dd37aa89b6464b94ff6
LandenKent <landokent73@gmail.com>
2022-08-26 17:09:41 -0400

    move file

--- Moves.py
+++ Moves.py
@@ -1,6 +1,17 @@
-class Moves
+class Moves:
     """
     description of stuff
     """
-    def __init__(self,damage,type,bonuseffects=(none)):
+    def __init__(self,damage,type,bonuseffects="none"):
+        self.damage=damage
+        self.type=type
+        self.bonuseffects=bonuseffects
 
+    def getdamage(self):
+        return(self.damage)
+
+    def gettype(self):
+        return(self.type)
+
+    def getbonuseffects(self):
+        return(self.bonuseffects)
