de6e44e78cb96c6d59eb9feea12e20af8df5ebf0
LandenKent <landokent73@gmail.com>
2022-08-26 16:53:29 -0400

    Area and Move files added

---
+++ Area.py
@@ -0,0 +1,17 @@
+class Area
+    """"
+    fun stuff here
+    """
+    def __init__(self):
+        self.weather
+        self.terrain
+        self.availablepokemon
+
+    def getterrain(self):
+        return(self.terrain)
+
+    def getweather(self):
+        return(self.weather)
+
+    def getavailablepokemon(self):
+        return(self.available_pokemon)
